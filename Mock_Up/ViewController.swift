00546bb6c1fafca79063e1572b32f55612afe0e4
Jonathan Lai <jonathanlai119@gmail.com>
2020-03-25 21:15:03 -0400

    Added Hello World
    Hello World

--- Mock_Up/ViewController.swift
+++ Mock_Up/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Hello World")
         // Do any additional setup after loading the view.
     }
 

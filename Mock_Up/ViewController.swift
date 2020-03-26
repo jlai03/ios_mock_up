9fef16796174da841910e4e6162dc4321a8e49cc
Jonathan Lai <jonathanlai119@gmail.com>
2020-03-25 21:09:55 -0400

    IOS Mock up
    Initial Code - for ios app mockup

---
+++ Mock_Up/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Mock_Up
+//
+//  Created by Jonathan Lai on 3/22/20.
+//  Copyright © 2020 Silk Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
+
---
+++ Mock_Up/captureViewController.swift
@@ -0,0 +1,12 @@
+//
+//  captureViewController.swift
+//  Mock_Up
+//
+//  Created by Jonathan Lai on 3/25/20.
+//  Copyright © 2020 Silk Lab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+

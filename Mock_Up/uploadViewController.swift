8d8d1ffdd86c2bfa660b7657efdda75ec7261d29
reshadbinharun <reshadbinharun@gmail.com>
2020-04-07 19:42:03 -0400

    Added action to send http request to upload image

--- Mock_Up/uploadViewController.swift
+++ Mock_Up/uploadViewController.swift
@@ -15,6 +15,11 @@ class uploadViewController: UIViewController {
 
     @IBOutlet weak var myImageview2: UIImageView!
     
+    @IBAction func upload(_ sender: Any) {
+        // TODO: connect this IB action to upload button
+        sendImageTimestamp(image: theImage2, filename: "NewImage")
+    }
+    
     
     override func viewDidLoad() {
            super.viewDidLoad()
@@ -34,14 +39,59 @@ class uploadViewController: UIViewController {
               
           }
     }
-    /*
-    // MARK: - Navigation
+    // Code for sending httpPost request
+    func sendImageTimestamp(image: UIImage, filename: String) {
+        // 2nd argument ins compression quality [0, 1.0]
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+            print("Unable to form JPEG representation")
+            return
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let url = URL(string: "http://localhost:3000/camera/sendImage")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        request.timeoutInterval = 30.0
+
+        let CRLF = "\r\n"
+        let formName = "file"
+        let type = "image/jpeg"     // file type
+        let boundary = String(format: "----iOSURLSessionBoundary.%08x%08x", arc4random(), arc4random())
+        var body = Data()
+
+        // file data //
+        body.append(("--\(boundary)" + CRLF).data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"\(formName)\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
+        body.append(("Content-Type: \(type)" + CRLF + CRLF).data(using: .utf8)!)
+        body.append(imageData as Data)
+        body.append(CRLF.data(using: .utf8)!)
+
+        // footer //
+        body.append(("--\(boundary)--" + CRLF).data(using: .utf8)!)
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        request.httpBody = body
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+
+                    if let json = jsonSerialized {
+                        print(json)
+                        let imageSuccessAlert =  UIAlertController(title: "Image Upload Successful!", message: json["message"] as? String, preferredStyle: .actionSheet)
+                        self.present(imageSuccessAlert, animated: true, completion: nil)
+                    }
+                }  catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+
+        task.resume()
     }
-    */
-
 }
